fd49ea9b3a10e1f0950a09114a3da33259f1fefa
George O. Wood <hello@gwood.dev>
2025-10-17 17:14:04 +0100

    Refactor on main.rs.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@ mod flags;
 #[macro_use]
 extern crate rocket;
 
+use flags::{check_flag, gen_flag};
+
 #[get("/")]
 fn index() -> String {
-    let flag = flags::gen_flag("12345678", 1);
-    let check_true = flags::check_flag(&flag, "12345678", 1);
-    let check_false = flags::check_flag(&flag, "87654321", 1);
+    let flag = gen_flag("12345678", 1);
+    let check_true = check_flag(&flag, "12345678", 1);
+    let check_false = check_flag(&flag, "87654321", 1);
 
-    format!("Flag: {}\nCheck True: {}\nCheck False: {}", flag, check_true, check_false)
+    format!(
+        "Flag: {}\nCheck True: {}\nCheck False: {}",
+        flag, check_true, check_false
+    )
 }
 
 #[launch]
