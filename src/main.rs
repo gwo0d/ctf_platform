925551394398979afbfa8428db6db27367004996
George O. Wood <hello@gwood.dev>
2025-10-17 17:12:33 +0100

    Update main.rs with rudimentary tests.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ extern crate rocket;
 
 #[get("/")]
 fn index() -> String {
-    flags::gen_flag("12345678", 1)
+    let flag = flags::gen_flag("12345678", 1);
+    let check_true = flags::check_flag(&flag, "12345678", 1);
+    let check_false = flags::check_flag(&flag, "87654321", 1);
+
+    format!("Flag: {}\nCheck True: {}\nCheck False: {}", flag, check_true, check_false)
 }
 
 #[launch]
